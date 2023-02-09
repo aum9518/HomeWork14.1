2ef3a281587de858ae82cd6d09be6b17f8e8b72c
aum9518 <abduganiuulumanasbek@gmail.com>
2023-02-09 13:47:34 +0600

    Initial commit

---
+++ src/Browser.java
@@ -0,0 +1,14 @@
+public class Browaer {
+    String name;
+
+    public Browaer() {
+    }
+
+    public Browaer(String name) {
+        this.name = name;
+    }
+
+    public void browser(){
+        System.out.println("Browser opened");
+    }
+}
